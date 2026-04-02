9db3c5fd251ef5c732853eb23e09080d4e4e3d9f
RONALD1510 <ronaldkumar1510@gmail.com>
2026-04-02 14:22:38 +0530

    Update Jenkinsfile1.groovy

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -4,7 +4,7 @@ pipeline {
         stage('Checkout') {
             steps {
                 // Use the 'git' step with a 'url' parameter
-                git url: https://github.com/RONALD1510/my-first-repo.git' branch: 'main'
+                git url: 'https://github.com/RONALD1510/my-first-repo' branch: 'main'
             }
         }
     stage('Build Image'){
